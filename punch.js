4427ce7a8adcf63ac8d32e7194d11d1ee9dc1344
amarupd <amarduttupadhyay@gmail.com>
2023-01-27 13:25:47 +0530

    commit

--- punch.js
+++ punch.js
@@ -1,6 +1,33 @@
-const empId='10';
+const { Sequelize, DataTypes } = require('sequelize')
 
-const data= (sequelize, DataTypes) => {
+
+const dbConfig = require('./config/dbConfig')
+const sequelize = new Sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.DIALECT,
+    operatorAliases: false,
+    pool: {
+        max: dbConfig.pool.max,
+        min: dbConfig.pool.min,
+        acquire: dbConfig.pool.acquire,
+        idle: dbConfig.pool.idle
+    }
+})
+sequelize.authenticate()
+    .then(() => {
+        console.log("database is connected");
+    })
+    .catch((err) => {
+        console.log("error :-" + err);
+    })
+
+
+const empId = '10';
+
+const data = (sequelize, DataTypes) => {
     const Punch = sequelize.define(`emp${empId}s`, {
         date: {
             type: DataTypes.DATEONLY,
@@ -25,3 +52,14 @@ const data= (sequelize, DataTypes) => {
     })
     return Punch;
 }
+
+
+const db = data
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+
+db.sequelize.sync({ force: false })
+    .then(() => {
+        console.log('re-sync done');
+    })
